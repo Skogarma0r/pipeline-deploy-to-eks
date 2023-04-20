a5904f1830803c1b1d29718ca34e39dabea7601d
Skogarma0r <v.voyteshonok@gmail.com>
2023-04-20 03:08:23 +0300

    Initial Commit

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,12 +1,10 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "SkogarmaorDiplomaCluster"
+    cluster_name = "myapp-eks-cluster"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
-
-    cluster_encryption_config = {}
 
     vpc_id = module.myapp-vpc.vpc_id
     subnet_ids = module.myapp-vpc.private_subnets
